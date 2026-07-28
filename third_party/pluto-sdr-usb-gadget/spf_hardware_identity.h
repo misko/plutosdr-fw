6938a41f0cb4f3c411efa1b5f1ffa74bf50b0625
misko <misko@cs.toronto.edu>
2026-07-28 01:39:10 +0100

    Use passive SPI-NOR identity without HDL changes

--- third_party/pluto-sdr-usb-gadget/spf_hardware_identity.h
+++
@@ -1,9 +0,0 @@
-#ifndef __SPF_HARDWARE_IDENTITY_H__
-#define __SPF_HARDWARE_IDENTITY_H__
-
-#include <stdbool.h>
-#include <stdint.h>
-
-bool spf_read_fpga_device_dna(uint64_t *device_dna);
-
-#endif
