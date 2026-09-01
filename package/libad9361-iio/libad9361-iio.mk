1c337a0b8d8126c9d1ed785607bc5ea52e7fed22
misko <misko@cs.toronto.edu>
2026-09-01 06:08:22 +0000

    build: support legacy packages with CMake 4

--- package/libad9361-iio/libad9361-iio.mk
+++ package/libad9361-iio/libad9361-iio.mk
@@ -10,5 +10,9 @@ LIBAD9361_IIO_INSTALL_STAGING = YES
 LIBAD9361_IIO_LICENSE = LGPL-2.1+
 LIBAD9361_IIO_LICENSE_FILES = LICENSE
 LIBAD9361_IIO_DEPENDENCIES = libiio
+# libad9361-iio 0.2 predates CMake 3.5.  CMake 4 removed implicit
+# compatibility with older policy versions, so select the minimum policy
+# version explicitly without changing the package's own build files.
+LIBAD9361_IIO_CONF_OPTS = -DCMAKE_POLICY_VERSION_MINIMUM=3.5
 
 $(eval $(cmake-package))
--- package/libini/libini.mk
+++ package/libini/libini.mk
@@ -11,5 +11,8 @@ LIBINI_SITE_METHOD = git
 LIBINI_INSTALL_STAGING = YES
 LIBINI_LICENSE = LGPLv2.1+
 LIBINI_LICENSE_FILES = LICENSE.txt
+# libini predates CMake 3.5.  CMake 4 requires the compatibility policy
+# floor to be selected explicitly for otherwise unchanged legacy projects.
+LIBINI_CONF_OPTS = -DCMAKE_POLICY_VERSION_MINIMUM=3.5
 
 $(eval $(cmake-package))
