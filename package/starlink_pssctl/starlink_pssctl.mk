875844d0dd2c2e9fa6cbf27f4fc7001f853e9d56
misko <misko@cs.toronto.edu>
2026-09-02 01:57:19 +0000

    pluto: package experimental PSS controller

---
+++ package/starlink_pssctl/starlink_pssctl.mk
@@ -0,0 +1,29 @@
+################################################################################
+#
+# starlink_pssctl -- experimental do-not-merge firmware only
+#
+################################################################################
+
+STARLINK_PSSCTL_SITE = $(TOPDIR)/../tools/starlink_pssctl
+STARLINK_PSSCTL_SITE_METHOD = local
+STARLINK_PSSCTL_LICENSE = GPL-2.0-or-later
+
+define STARLINK_PSSCTL_BUILD_CMDS
+	$(TARGET_CC) $(TARGET_CFLAGS) $(TARGET_LDFLAGS) \
+		-std=c11 -Wall -Wextra -Werror -Wpedantic \
+		$(@D)/starlink_pss_hw.c $(@D)/starlink_pssctl.c \
+		-o $(@D)/starlink_pssctl
+endef
+
+define STARLINK_PSSCTL_INSTALL_TARGET_CMDS
+	$(INSTALL) -D -m 0755 $(@D)/starlink_pssctl \
+		$(TARGET_DIR)/usr/sbin/starlink_pssctl
+	$(INSTALL) -D -m 0644 \
+		$(TOPDIR)/../hdl/library/starlink_pss_raw_correlator/tb/upper_minus100k_coefficients_q15.mem \
+		$(TARGET_DIR)/opt/starlink-pss/upper_minus100k_coefficients_q15.mem
+	$(INSTALL) -D -m 0644 \
+		$(TOPDIR)/../hdl/library/axi_starlink_pss_tracker/tb/real_071200_wrapper_replay_provenance.json \
+		$(TARGET_DIR)/opt/starlink-pss/real_071200_wrapper_replay_provenance.json
+endef
+
+$(eval $(generic-package))
