6971a8c1fb5f8422cf5f32e239efe49c2cea08ec
misko <misko@cs.toronto.edu>
2026-09-03 18:18:59 +0000

    pluto: package experimental PSS acquisition controller

--- package/starlink_pssctl/starlink_pssctl.mk
+++ package/starlink_pssctl/starlink_pssctl.mk
@@ -13,11 +13,17 @@ define STARLINK_PSSCTL_BUILD_CMDS
 		-std=c11 -Wall -Wextra -Werror -Wpedantic \
 		$(@D)/starlink_pss_hw.c $(@D)/starlink_pssctl.c \
 		-o $(@D)/starlink_pssctl
+	$(TARGET_CC) $(TARGET_CFLAGS) $(TARGET_LDFLAGS) \
+		-std=c11 -Wall -Wextra -Werror -Wpedantic \
+		$(@D)/starlink_pss_acquisition.c $(@D)/starlink_pss_acqctl.c \
+		-lm -o $(@D)/starlink_pss_acqctl
 endef
 
 define STARLINK_PSSCTL_INSTALL_TARGET_CMDS
 	$(INSTALL) -D -m 0755 $(@D)/starlink_pssctl \
 		$(TARGET_DIR)/usr/sbin/starlink_pssctl
+	$(INSTALL) -D -m 0755 $(@D)/starlink_pss_acqctl \
+		$(TARGET_DIR)/usr/sbin/starlink_pss_acqctl
 	$(INSTALL) -D -m 0644 \
 		$(TOPDIR)/../hdl/library/starlink_pss_raw_correlator/tb/upper_minus100k_coefficients_q15.mem \
 		$(TARGET_DIR)/opt/starlink-pss/upper_minus100k_coefficients_q15.mem
